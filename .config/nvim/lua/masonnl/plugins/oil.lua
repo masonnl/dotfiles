65efed7dca22001d6f6fe7e005f23efb77b355dd
Mason <masonnl@yahoo.com>
2025-06-03 14:33:35 -0600

    add: oil nvim should show hidden files

--- .config/nvim/lua/masonnl/plugins/oil.lua
+++ .config/nvim/lua/masonnl/plugins/oil.lua
@@ -1,7 +1,13 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {
-		show_hidden = true,
-	},
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+  "stevearc/oil.nvim",
+  opts = {
+    view_options = {
+      show_hidden = true,
+      is_hidden_file = function(name)
+        local m = name:match("^%.")
+        return m ~= nil
+      end,
+    }
+  },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
 }
