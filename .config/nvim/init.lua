fc628523d4f8cc4de66c4d3c02376b75060796d9
Mason <masonnl@yahoo.com>
2025-09-10 12:02:26 -0600

    remove(nvim): Neovim is now completely managed by github://masonnl/masonnl.nvim

--- .config/nvim/init.lua
+++
@@ -1,2 +0,0 @@
-require("masonnl.core")
-require("masonnl.lazy")
--- .config/nvim/lua/masonnl/plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	opts = {
-		direction = "horizontal",
-		open_mapping = [[<c-\>]],
-		size = 20,
-	},
-}
